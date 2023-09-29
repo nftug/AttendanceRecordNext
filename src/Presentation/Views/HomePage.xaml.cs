579ec8d85f1a11934f13fbc524eabfc111db6cd0
V-nyang <whitepony1214@gmail.com>
2023-09-29 20:43:01 +0900

    Adding navigation view TODO: エラー解消

---
+++ src/Presentation/Views/HomePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Presentation.Views
+{
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- src/Presentation/Views/MainWindow.xaml.cs
+++ src/Presentation/Views/MainWindow.xaml.cs
@@ -1,15 +1,59 @@
-﻿using System.Windows;
+﻿using ModernWpf.Controls;
+using System.Windows;
 
 namespace Presentation.Views
 {
+    public enum NavigationIcon
+    {
+        Home,
+        // History,
+        // Setting,
+        None
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static IReadOnlyDictionary<NavigationIcon, Type> _pages = new Dictionary<NavigationIcon, Type>()
+        {
+            { NavigationIcon.Home, typeof(HomePage) },
+            { NavigationIcon.None, typeof(HomePage) }
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            try
+            {
+                var selectedItem = (NavigationViewItem)args.SelectedItem;
+                
+                // Tag取得
+                string? iconName = selectedItem.Tag?.ToString();
+
+                // ヘッダー設定
+                sender.Header = iconName;
+
+                if (Enum.TryParse(iconName, out NavigationIcon icon))
+                {
+                    // 対応するページを表示
+                    ContentFrame.Navigate(_pages[icon]);
+                }
+                else
+                {
+                    // 空ページ表示
+                    ContentFrame.Navigate(_pages[NavigationIcon.None]);
+                }
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            }
         }
     }
 }
