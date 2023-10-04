65cb0653245bb07eef071649d4a0a7f559804c77
V-nyang <whitepony1214@gmail.com>
2023-10-04 15:07:32 +0900

    Separate editing screen into another control

---
+++ src/Presentation/Views/HistoryItemView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Presentation.Views
+{
+    /// <summary>
+    /// HistoryItemView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class HistoryItemView : System.Windows.Controls.UserControl
+    {
+        public HistoryItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
